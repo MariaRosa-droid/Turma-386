d929ae9a02605244e68d807000c0168830ff76c2
MariaRosa <dorynha26@gmail.com>
2019-09-26 19:00:47 -0300

    projeto2

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Pedido
+
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('tipo_categoria', 'precoMaximo')
+
+admin.site.register(Pedido, PedidoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class  Pedido(models . Model):
+    tipo_categoria = models.CharField(max_length=50)
+    descricao = models.CharField(max_length = 80)
+    qtdPrevistoMes = models.FloatField ()
+    preco = models.FloatField ()
+    precoMaximo = models.FloatField ()
+
+    def __str__(self):
+        return self.tipo_categoria
